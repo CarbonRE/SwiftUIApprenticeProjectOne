ecdf918f2eff9766cc8dc859c0561deb72cf7f36
CarbonRE <carbonremn@gmail.com>
2023-08-21 18:04:18 +0200

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Vera Remn on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
